f064e0186dc399e11d448a20d3b182360392f3b6
Matias <153396776+MatiasNahuelhual@users.noreply.github.com>
2024-04-14 11:02:10 -0300

    Update sea_level_predictor.py

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,33 +4,33 @@ from scipy.stats import linregress
 import numpy as np
 
 def draw_plot():
-    # Read data from file
+    # Leer los datos 
     df = pd.read_csv('epa-sea-level.csv')
-
-    # Create scatter plot
-    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
-
-    # Create first line of best fit
+    
+    # Crear diagrama de dispersión
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], c=df['Year'], cmap='viridis')
+    
+    # Crear primera línea de mejor ajuste
     lineA = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
     xA = np.arange(df['Year'].min(),2050,1)
     yA = xA*lineA.slope + lineA.intercept
-
-    plt.plot(xA,yA)
-
-    # Crear una segunda línea de mejor ajuste
+    
+    plt.plot(xA,yA, color='orange')
+    
+    # Crear segunda línea de mejor ajuste
     df_recent = df[df['Year'] >= 2000]
-
+    
     lineB = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
     xB = np.arange(2000,2050,1)
     yB = xB*lineB.slope + lineB.intercept
-
-    plt.plot(xB,yB)
-
-    # Add labels and title
+    
+    plt.plot(xB,yB, color='red')
+    
+    # Agregar títulos y guardar la imagen
     plt.xlabel('Year')
     plt.ylabel('Sea Level (inches)')
     plt.title('Rise in Sea Level')
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
+    plt.colorbar(label='Year')
+            
     plt.savefig('sea_level_plot.png')
     return plt.gca()
